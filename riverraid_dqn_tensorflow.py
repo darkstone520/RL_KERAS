7375896f0304cccfa0bf357ea02189461cbaf61e
wisdona <wisdona520@gmail.com>
2017-08-20 01:25:15 +0900

    keras

--- riverraid_dqn_tensorflow.py
+++ riverraid_dqn_tensorflow.py
@@ -25,7 +25,7 @@ TARGET_UPDATE_FREQUENCY = 10000  # 몇 Frame 마다 Target 신경망을 업데�
 MAX_EPISODES = 50000             # 게임을 플레이 할 최대 EPISODE
 START_EXPLORATION = 1.0          # Epsilon 시작 값 (Exploration and Exploit Greedy 설정 관련 변수)
 FINAL_EXPLORATION = 0.1          # Epsilon 마지막 값 (Exploration and Exploit Greedy 설정 관련 변수)
-EXPLORATION = 1500000            # Epsilon 시작부터 마지막 값까지 몇개의 step(time_step, frame, action)으로 줄여나갈 지 정하는 변수
+EXPLORATION = 1300000            # Epsilon 시작부터 마지막 값까지 몇개의 step(time_step, frame, action)으로 줄여나갈 지 정하는 변수
 
 
 
@@ -128,7 +128,7 @@ if __name__ == "__main__":
 
                 # Epsilon이 1보다 작아지는 순간부터 랜덤의 확률로 random action 또는 모델 predict action을 취함
                 if np.random.rand() < e:
-                    action = np.random.choice(np.arange(7))
+                    action = np.random.choice(np.arange(4))
                 else:
                     # Choose an action by greedily from the Q-network
                     action = np.argmax(mainDQN.predict(history/255.))
