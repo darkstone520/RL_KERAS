d5895fbfa3c2c4e6fafebbb25f7ade70b7899af3
wisdona <wisdona520@gmail.com>
2017-09-02 12:00:29 +0900

    keras

--- model_kkc_main.py
+++ model_kkc_main.py
@@ -28,7 +28,7 @@ def loadInputData():
         file.close()
         return lines[:train_last_index], lines[train_last_index:]
 
-def readData(lines):
+def readData(lines, START_BATCH_INDEX):
     print("line seperating")
     # data = [line.split(',')[:-1] for line in lines]
     data = [line.split(',')[:-1] for line in lines]
@@ -36,7 +36,8 @@ def readData(lines):
     print("array 변환중")
     # data = np.loadtxt(data, dtype=np.uint8)
     data = np.array(data, dtype=np.uint8)
-    data, label = data[:,:-1], data[:,-1]
+    data, label = data[START_BATCH_INDEX+BATCH_SIZE,:-1], data[START_BATCH_INDEX+BATCH_SIZE:,-1]
+    START_BATCH_INDEX += BATCH_SIZE
     return data, label
 
 __DATA_PATH = "preprocessed_data/"
@@ -44,18 +45,18 @@ __DATA_PATH = "preprocessed_data/"
 IMG_SIZE = (144,144)
 
 BATCH_SIZE = 100
+START_BATCH_INDEX = 0
 TRAIN_EPOCHS = 20
 TRAIN_RATE = 0.8
 NUM_MODELS = 5
 LEARNING_RATE = 0.0001
 
-TRAIN_DATA_X, TEST_DATA_X = loadInputData()
+TRAIN_DATA, TEST_DATA = loadInputData()
 
-print("Train Reading Data")
-TRAIN_DATA_X, TRAIN_DATA_Y = readData(TRAIN_DATA_X)
-
-print("Test Reading Data")
-TEST_DATA_X, TEST_DATA_Y = readData(TEST_DATA_X)
+# print("Train Reading Data")
+# TRAIN_DATA_X, TRAIN_DATA_Y = readData(TRAIN_DATA_X)
+# print("Test Reading Data")
+# TEST_DATA_X, TEST_DATA_Y = readData(TEST_DATA_X)
 
 print("session open")
 # initialize
@@ -73,8 +74,9 @@ print('Learning Started!')
 # train my model
 for epoch in range(TRAIN_EPOCHS):
     avg_cost_list = np.zeros(len(models))
-    total_batch = round(int(len(TRAIN_DATA_X) / BATCH_SIZE))
+    total_batch = round(int(len(TRAIN_DATA) / BATCH_SIZE))
     for i in range(total_batch):
+        TRAIN_DATA_X, TRAIN_DATA_Y = readData(TRAIN_DATA)
         # train each model
         for m_idx, m in enumerate(models):
             c, _ = m.train(TRAIN_DATA_X, TRAIN_DATA_Y)
@@ -86,18 +88,18 @@ print('Learning Finished!')
 
 
 # Test model and check accuracy
-test_size = len(TEST_DATA_Y)
-predictions = np.zeros(test_size * 2).reshape(test_size, 2)
-for m_idx, m in enumerate(models):
-    print(m_idx, 'Accuracy:', m.get_accuracy(TEST_DATA_X, TEST_DATA_Y))
-    p = m.predict(TEST_DATA_X)
-    predictions += p
-
-ensemble_correct_prediction = tf.equal(
-    tf.argmax(predictions, 1), tf.argmax(TEST_DATA_Y, 1))
-ensemble_accuracy = tf.reduce_mean(
-    tf.cast(ensemble_correct_prediction, tf.float32))
-print('Ensemble accuracy:', sess.run(ensemble_accuracy))
+# test_size = len(TEST_DATA)
+# predictions = np.zeros(test_size * 2).reshape(test_size, 2)
+# for m_idx, m in enumerate(models):
+#     print(m_idx, 'Accuracy:', m.get_accuracy(TEST_DATA_X, TEST_DATA_Y))
+#     p = m.predict(TEST_DATA_X)
+#     predictions += p
+#
+# ensemble_correct_prediction = tf.equal(
+#     tf.argmax(predictions, 1), tf.argmax(TEST_DATA_Y, 1))
+# ensemble_accuracy = tf.reduce_mean(
+#     tf.cast(ensemble_correct_prediction, tf.float32))
+# print('Ensemble accuracy:', sess.run(ensemble_accuracy))
 
 
 
