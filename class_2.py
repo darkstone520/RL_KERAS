b2457694aae17cc86a946ee4a53c857e5f7dc725
wisdona <wisdona520@gmail.com>
2017-09-16 11:13:52 +0900

    keras

--- class_2.py
+++ class_2.py
@@ -359,7 +359,7 @@ with tf.Session() as sess:
 
             TEST_ACCURACY_LIST.append(TEST_ACCURACY)
             if len(TEST_ACCURACY_LIST) != 1:
-                if float(TEST_ACCURACY_LIST[0]) >= float(TEST_ACCURACY_LIST[1]):
+                if abs(float(TEST_ACCURACY_LIST[0] - TEST_ACCURACY_LIST[1])) > 0.01 and  float(TEST_ACCURACY_LIST[0]) >= float(TEST_ACCURACY_LIST[1]):
                     print("Ealry Stop 으로 학습을 중단합니다.")
                     print("최고정확도 {}".format(TEST_ACCURACY_LIST[0]))
                     break
