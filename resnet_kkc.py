91950c2076888809123e1e819909b015ea405d53
wisdona <wisdona520@gmail.com>
2017-09-16 21:53:12 +0900

    keras

--- resnet_kkc.py
+++ resnet_kkc.py
@@ -159,11 +159,11 @@ class Model:
         ################################################################################################################
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y)) + (0.01/(2*tf.to_float(tf.shape(self.Y)[0])))*tf.reduce_sum(tf.square(self.W_out))
 
-        self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025,
-                                                   momentum=0.9, decay=0.99,
-                                                   epsilon=0.01).minimize(self.cost)
+        # self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025,
+        #                                            momentum=0.9, decay=0.99,
+        #                                            epsilon=0.01).minimize(self.cost)
 
-        # self.optimizer = tf.train.AdamOptimizer(learning_rate=0.005).minimize(self.cost)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0025).minimize(self.cost)
 
         self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(self.logits, 1), tf.arg_max(self.Y, 1)), dtype=tf.float32))
 
