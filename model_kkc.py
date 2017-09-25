add6c44e3f5bf823caf74c4b92e61039ec0036d3
wisdona <wisdona520@gmail.com>
2017-09-26 00:19:27 +0900

    desktop

--- model_kkc.py
+++ model_kkc.py
@@ -236,7 +236,7 @@ class Model:
         ################################################################################################################
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y)) \
                     + (0.01/(2*tf.to_float(tf.shape(self.Y)[0])))*tf.reduce_sum(tf.square(self.W_out)) \
-                    + (0.01/(2*tf.to_float(tf.shape(self.Y)[0])))*tf.reduce_sum(tf.square(self.W_fc2))
+                    + (0.01 / (2 * tf.to_float(tf.shape(self.Y)[0]))) * tf.reduce_sum(tf.square(self.W_fc2))
 
         self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025,
                                                    momentum=0.9, decay=0.99,
