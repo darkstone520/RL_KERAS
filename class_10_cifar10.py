c77b742a7441f2a574f6f38b76c6aef2a50246ee
wisdona <wisdona520@gmail.com>
2017-09-27 14:51:25 +0900

    ``

--- class_10_cifar10.py
+++ class_10_cifar10.py
@@ -473,8 +473,8 @@ with tf.Session() as sess:
                     # 위에서 load배치 함수를 호출하면 START_BATCH_INDEX가 BATCH_SIZE만큼 증가하기 때문에 다시 빼준다.
                     predictions[START_BATCH_INDEX-BATCH_SIZE:START_BATCH_INDEX,:] += p
                 print(START_BATCH_INDEX, BATCH_SIZE)
-                print(predictions)
-                print(test_y_batch)
+                print(len(predictions[START_BATCH_INDEX-BATCH_SIZE:START_BATCH_INDEX,:]))
+                print(len(test_y_batch[START_BATCH_INDEX-BATCH_SIZE:START_BATCH_INDEX,:]))
                 ensemble_batch_correct_prediciton = tf.equal(tf.argmax(predictions[START_BATCH_INDEX-BATCH_SIZE:START_BATCH_INDEX,:], 1),
                                                              tf.argmax(test_y_batch[START_BATCH_INDEX-BATCH_SIZE:START_BATCH_INDEX,:],1))
 
