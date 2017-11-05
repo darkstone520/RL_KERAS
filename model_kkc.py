b3b2894ed7703b4f516f8d7f2590c36111d2553c
wisdona <wisdona520@gmail.com>
2017-11-05 18:41:20 +0900

    ``

--- model_kkc.py
+++ model_kkc.py
@@ -1,67 +1,4 @@
-########################################################################################################################
-## ▣ 가중치, 편향 파라미터 초기화
-##  - weight 는 layer 의 입출력 node 수에 따라 적응적으로 normal distribution 의 variance 를 정해주는 것이 좋다.
-##  - Bias 는 아주 작은 상수값으로 초기화 해주는 것이 낫다.
-##  - 따라서, weight 초기화 방법 후보로 normal, truncated_normal, xavier, he 방법을 선정하고,
-##    bias 초기화 방법 후보로 normal, zero 방법을 선정하였다.
-##  - no batch normalization 인 경우 he weight 에 bias 0 으로 초기화 한 경우가 가장 성능이 좋았다.
-##  - batch normalization 인 경우에는 no batch normalization 인 경우보다 He 초기값인 경우 약 3~4 % 정도 성능 향상이 있다.
-##  ⊙ 초기화 방법
-##   1. with constant
-##    - tf.Variable(tf.zeros([784, 10])) : 0 으로 초기화
-##    - tf.Variable(tf.constant(0.1, [784, 10])) : 0.1 로 초기화
-##   2. with normal distribution
-##    - tf.Variable(tf.random_normal([784, 10])) : 평균 0, 표준편차 1 인 정규분포 값
-##   3. with truncated normal distribution
-##    - tf.truncated_normal([784, 10], stddev=0.1) : 평균 0, 표준편차 0.1 인 정규분포에서 샘플링 된 값이 2*stddev 보다 큰 경우 해당 샘플을 버리고 다시 샘플링하는 방법.
-##   4. with Xavier initialization
-##    - tf.get_variable('w1', shape=[784, 10], initializer=tf.contrib.layers.xavier_initializer())
-##   5. with He initialization
-##    - tf.get_variable('w1', shape=[784, 10], initializer=tf.contrib.layers.variance_scaling_initializer())
-##
-## ▣ tf.nn.conv2d(
-##   input,                  : 4-D 입력 값 [batch, in_height, in_width, in_channels]
-##   filter,                 : 4-D 필터 값 [filter_height, filter_width, in_channels, out_channels]
-##   strides,                : 길이 4의 1-D 텐서. (4차원 입력이어서 각 차원마다 스트라이드 값을 설정), 기본적으로 strides = [1, stride, stride, 1] 로 설정한다.
-##   padding,                : 'SAME' or 'VALID' 둘 중의 하나의 값을 가진다. (스트라이드가 1x1 인 경우에만 동작.)
-##   use_cudnn_on_gpu=None,  : GPU 사용에 대한 bool 값.
-##   data_format=None,       : 'NHWC' : [batch, height, width, channels], 'NCHW' : [batch, channels, height, width]
-##   name=None               : 연산에 대한 이름 설정.
-##   )
-##  1. 2-D matrix 형태로 필터를 납작하게 만든다. (filter_height * filter_width * in_channels, output_channels]
-##  2. 가상 텐서 형태로 형상화하기 위해 입력 텐서로부터 이미지 패치들을 추출한다. [batch, out_height, out_width, filter_height * filter_width * in_channels]
-##  3. 각 패치에 대해 필터 행렬과 이미지 패치 벡터를 오른쪽으로 행렬곱 연산을 수행한다.
-##
-## ▣ tf.nn.max_pool(
-##   value,             : 4-D 텐서 형태 [batch, height, width, channels], type : tf.float32
-##   ksize,             : 입력 값의 각 차원에 대한 윈도우 크기.
-##   strides,           : 입력 값의 각 차원에 대한 sliding 윈도우 크기.
-##   padding,           : 'SAME' :  output size => input size, 'VALID' : output size => ksize - 1
-##   data_format='NHWC' : 'NHWC' : [batch, height, width, channels], 'NCHW' : [batch, channels, height, width]
-##   name=None          : 연산에 대한 이름 설정.
-##   )
-##  1. 입력 값에 대해 윈도우 크기 내에서의 가장 큰 값을 골라서 차원을 축소 시키는 함수.
-##
-## ▣ 경사 감소법
-##  1. SGD : 이전 가중치 매개 변수에 대한 손실 함수 기울기는 수치 미분을 사용해 구하고 기울기의 학습률만큼 이동하도록 구현하는 최적화 알고리즘.
-##           wi ← wi ? η(∂E / ∂wi), η : 학습률
-##   - tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
-##  2. Momentum
-##   - tf.train.MomentumOptimizer
-##  3. AdaGrad
-##   - tf.train.AdagradOptimizer
-##  4. ADAM
-##   - tf.train.AdamOptimizer
-##  5. Adadelta
-##   - tf.train.AdadeltaOptimizer
-##  6. RMSprop
-##   - tf.train.RMSPropOptimizer
-##  7. Etc
-##   - tf.train.AdagradDAOptimizer
-##   - tf.train.FtrlOptimizer
-##   - tf.train.ProximalGradientDescentOptimizer
-##   - tf.train.ProximalAdagradOptimizer
-########################################################################################################################
+
 import tensorflow as tf
 import numpy as np
 import time
@@ -76,12 +13,7 @@ class Model:
     def _build_net(self):
         with tf.variable_scope(self.name):
             with tf.name_scope('input_layer') as scope:
-                ########################################################################################################
-                ## ▣ Dropout
-                ##  - 랜덤으로 노드를 삭제하여 입력과 출력 사이의 연결을 제거하는 기법.
-                ##  - 모델이 데이터에 오버피팅 되는 것을 막아주는 역할.
-                ##  ⊙ 학습 : 0.5, 테스트 : 1
-                ########################################################################################################
+
                 self.dropout_rate = tf.Variable(tf.constant(value=0.5), name='dropout_rate')
                 self.training = tf.placeholder(tf.bool, name='training')
 
@@ -89,15 +21,6 @@ class Model:
                 X_img = tf.reshape(self.X, shape=[-1, 144, 144, 1])
                 self.Y = tf.placeholder(tf.float32, [None, self.class_num], name='y_data')
 
-            ############################################################################################################
-            ## ▣ Convolution 계층 - 1
-            ##  ⊙ 합성곱 계층 → filter: (7, 7), padding: VALID output: 20 개, 초기값: He
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 풀링 계층   → Max Pooling
-            ##  ⊙ 드롭 아웃 구현
-            ## tf.contrib.layers.variance_scaling_initializer()
-            ## tf.contrib.layers.xavier_initializer_conv2d()
-            ############################################################################################################
             with tf.name_scope('conv_layer1') as scope:
                 self.W1_sub = tf.get_variable(name='W1_sub', shape=[4, 4, 1, 20], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer_conv2d())
                 self.L1_sub = tf.nn.conv2d(input=X_img, filter=self.W1_sub, strides=[1, 1, 1, 1], padding='VALID')  # 144x144 -> 141x141
@@ -113,43 +36,23 @@ class Model:
                 self.L3_sub = tf.nn.conv2d(input=self.L2_sub, filter=self.W3_sub, strides=[1, 1, 1, 1], padding='VALID')  # 138x138 -> 136x136
                 self.L3_sub = self.BN(input=self.L3_sub, scale=True, training=self.training, name='Conv3_sub_BN')
                 self.L3_sub = self.parametric_relu(self.L3_sub, 'R3_sub')
-                ###################################################################################################################
-                ## ▣ Local Response Normalization 구현
-                ##  ⊙ conv 계층과 pool 계층 사이에 넣는 정규화 기법
-                ##  ⊙ depth_radius = conv layer 총 개수 , bias / alpha / beta 값은 임의의 파라미터(AlexNet 의 파라미터로 임시 지정)
-                ###################################################################################################################
+
                 # self.L1 = tf.nn.lrn(self.L3_sub, depth_radius=5, bias=2, alpha=0.0001, beta=0.75, name='LRN1')
                 self.L1 = tf.nn.max_pool(value=self.L3_sub, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 136x136 -> 68x68
                 # self.L1 = tf.layers.dropout(inputs=self.L1, rate=self.dropout_rate, training=self.training)
 
-            ############################################################################################################
-            ## ▣ Convolution 계층 - 2
-            ##  ⊙ 합성곱 계층 → filter: (3, 3), output: 40 개, 초기값: He
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 풀링 계층   → Max Pooling
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('conv_layer2') as scope:
                 self.W2 = tf.get_variable(name='W2', shape=[3, 3, 20, 40], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer_conv2d())
                 self.L2 = tf.nn.conv2d(input=self.L1, filter=self.W2, strides=[1, 1, 1, 1], padding='SAME')
                 self.L2 = self.BN(input=self.L2, scale=True, training=self.training, name='Conv2_BN')
                 self.L2 = self.parametric_relu(self.L2, 'R2')
-                ###################################################################################################################
-                ## ▣ Local Response Normalization 구현
-                ##  ⊙ conv 계층과 pool 계층 사이에 넣는 정규화 기법
-                ##  ⊙ depth_radius = conv layer 총 개수 , bias / alpha / beta 값은 임의의 파라미터(AlexNet 의 파라미터로 임시 지정)
-                ###################################################################################################################
+
                 # self.L2 = tf.nn.lrn(self.L2, depth_radius=5, bias=2, alpha=0.0001, beta=0.75, name='LRN2') # Local Response Normalization 구현
                 self.L2 = tf.nn.max_pool(value=self.L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 68x68 -> 34x34
                 # self.L2 = tf.layers.dropout(inputs=self.L2, rate=self.dropout_rate, training=self.training)
 
-            ############################################################################################################
-            ## ▣ Convolution 계층 - 3
-            ##  ⊙ 합성곱 계층 → filter: (3, 3), output: 80 개, 초기값: He
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 풀링 계층   → Max Pooling
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('conv_layer3') as scope:
                 self.W3 = tf.get_variable(name='W3', shape=[3, 3, 40, 80], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer_conv2d())
                 self.L3 = tf.nn.conv2d(input=self.L2, filter=self.W3, strides=[1, 1, 1, 1], padding='SAME')
@@ -158,13 +61,7 @@ class Model:
                 self.L3 = tf.nn.max_pool(value=self.L3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 34x34 -> 17x17
                 # self.L3 = tf.layers.dropout(inputs=self.L3, rate=self.dropout_rate, training=self.training)
 
-            ############################################################################################################
-            ## ▣ Convolution 계층 - 4
-            ##  ⊙ 합성곱 계층 → filter: (3, 3), output: 160 개, 초기값: He
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 풀링 계층   → Max Pooling
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('conv_layer4') as scope:
                 self.W4 = tf.get_variable(name='W4', shape=[2, 2, 80, 160], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer_conv2d())
                 self.L4 = tf.nn.conv2d(input=self.L3, filter=self.W4, strides=[1, 1, 1, 1], padding='VALID')
@@ -174,13 +71,7 @@ class Model:
                 # self.L4 = tf.layers.dropout(inputs=self.L4, rate=self.dropout_rate, training=self.training)
                 # self.L4 = tf.reshape(self.L4, shape=[-1, 8 * 8 * 160])
 
-            ############################################################################################################
-            ## ▣ Convolution 계층 - 5
-            ##  ⊙ 합성곱 계층 → filter: (3, 3), output: 320 개, 초기값: He
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 풀링 계층   → Max Pooling
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('conv_layer5') as scope:
                 self.W5 = tf.get_variable(name='W5', shape=[7, 7, 160, 320], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer_conv2d())
                 self.L5 = tf.nn.conv2d(input=self.L4, filter=self.W5, strides=[1, 1, 1, 1], padding='SAME')
@@ -190,13 +81,7 @@ class Model:
                 # self.L5 = tf.layers.dropout(inputs=self.L5, rate=self.dropout_rate, training=self.training)
                 self.L5 = tf.reshape(self.L5, shape=[-1, 4 * 4 * 320])
 
-            ############################################################################################################
-            ## ▣ fully connected 계층 - 1
-            ##  ⊙ 가중치      → shape: (4 * 4 * 320, 625), output: 625 개, 초기값: He
-            ##  ⊙ 편향        → shape: 625, 초기값: 0.001
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('fc_layer1') as scope:
                 self.W_fc1 = tf.get_variable(name='W_fc1', shape=[4 * 4 * 320, 1000], dtype=tf.float32, initializer=tf.contrib.layers.variance_scaling_initializer())
                 self.b_fc1 = tf.Variable(tf.constant(value=0.001, shape=[1000], name='b_fc1'))
@@ -205,13 +90,7 @@ class Model:
                 self.L_fc1 = self.parametric_relu(self.L6, 'R_fc1')
                 # self.L_fc1 = tf.layers.dropout(inputs=self.L_fc1, rate=self.dropout_rate, training=self.training)
 
-            ############################################################################################################
-            ## ▣ fully connected 계층 - 2
-            ##  ⊙ 가중치      → shape: (625, 625), output: 625 개, 초기값: He
-            ##  ⊙ 편향        → shape: 625, 초기값: 0.001
-            ##  ⊙ 활성화 함수 → Parametric Relu
-            ##  ⊙ 드롭 아웃 구현
-            ############################################################################################################
+
             with tf.name_scope('fc_layer2') as scope:
                 self.W_fc2 = tf.get_variable(name='W_fc2', shape=[1000, 1000], dtype=tf.float32, initializer=tf.contrib.layers.variance_scaling_initializer())
                 self.b_fc2 = tf.Variable(tf.constant(value=0.001, shape=[1000], name='b_fc2'))
@@ -220,20 +99,12 @@ class Model:
                 self.L_fc2 = self.parametric_relu(self.L7, 'R_fc2')
                 # self.L_fc2 = tf.layers.dropout(inputs=self.L_fc2, rate=self.dropout_rate, training=self.training)
 
-            ############################################################################################################
-            ## ▣ 출력층
-            ##  ⊙ 가중치      → shape: (625, 10), output: 2 개, 초기값: He
-            ##  ⊙ 편향        → shape: 2, 초기값: 0.001
-            ##  ⊙ 활성화 함수 → Softmax
-            ############################################################################################################
+
             self.W_out = tf.get_variable(name='W_out', shape=[1000, self.class_num], dtype=tf.float32, initializer=tf.contrib.layers.variance_scaling_initializer())
             self.b_out = tf.Variable(tf.constant(value=0.001, shape=[self.class_num], name='b_out'))
             self.logits = tf.matmul(self.L_fc2, self.W_out) + self.b_out
 
-        ################################################################################################################
-        ## ▣ L2-Regularization
-        ##  ⊙ λ/(2*N)*Σ(W)²-> (0.001/(2*tf.to_float(tf.shape(self.X)[0])))*tf.reduce_sum(tf.square(self.W7))
-        ################################################################################################################
+
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y)) \
                     + (0.01/(2*tf.to_float(tf.shape(self.Y)[0])))*tf.reduce_sum(tf.square(self.W_out))
         self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025,
@@ -246,11 +117,7 @@ class Model:
 
         # self.tensorflow_summary()
 
-    ####################################################################################################################
-    ## ▣ Tensorboard logging
-    ##  ⊙ tf.summary.histogram : 여러개의 행렬 값을 logging 하는 경우
-    ##  ⊙ tf.summary.scalar : 한개의 상수 값을 logging 하는 경우
-    ####################################################################################################################
+
     def tensorflow_summary(self):
         self.W1_hist = tf.summary.histogram('W1_conv1', self.W1)
         self.b1_hist = tf.summary.histogram('b1_conv1', self.b1)
@@ -303,12 +170,7 @@ class Model:
     def train(self, x_data, y_data):
         return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.training: True})
 
-    ####################################################################################################################
-    ## ▣ Parametric Relu or Leaky Relu
-    ##  ⊙ alpha 값을 설정해 0 이하인 경우 alpha 만큼의 경사를 설정해서 0 이 아닌 값을 리턴하는 함수
-    ##  ⊙ Parametric Relu : 학습을 통해 최적화된 alpha 값을 구해서 적용하는 Relu 함수
-    ##  ⊙ Leaky Relu      : 0 에 근접한 작은 값을 alpha 값으로 설정하는 Relu 함수
-    ####################################################################################################################
+
     def parametric_relu(self, _x, name):
         alphas = tf.get_variable(name,
                                  _x.get_shape()[-1],
@@ -318,12 +180,7 @@ class Model:
         neg = alphas * (_x - abs(_x)) * 0.5
         return pos + neg
 
-    ####################################################################################################################
-    ## ▣ Maxout
-    ##  ⊙ Convolution 계층이나 FC 계층에서 활성화 함수 대신 dropout 의 효율을 극대화하기 위해 사용하는 함수
-    ##  ⊙ conv 또는 affine 계층을 거친 값들에 대해 k 개씩 그룹핑을 수행하고 해당 그룹내에서 가장 큰 값을 다음 계층으로
-    ##     보내는 기법
-    ####################################################################################################################
+
     def max_out(self, inputs, num_units, axis=None):
         shape = inputs.get_shape().as_list()
         if shape[0] is None:
@@ -339,25 +196,7 @@ class Model:
         outputs = tf.reduce_max(tf.reshape(inputs, shape), -1, keep_dims=False)
         return outputs
 
-    ####################################################################################################################
-    ## ▣ Batch Normalization
-    ##  ⊙ training 하는 과정 자체를 전체적으로 안정화하여 학습 속도를 가속시킬 수 있는 방법
-    ##  ⊙ Network의 각 층이나 Activation 마다 input_data 의 distribution 을 평균 0, 표준편차 1인 input_data로 정규화시키는 방법
-    ##  ⊙ 초기 파라미터 --> beta : 0 , gamma : 1 , decay : 0.99 , epsilon : 0.001
-    ####################################################################################################################
     def BN(self, input, training, scale, name, decay=0.99):
         return tf.contrib.layers.batch_norm(input, decay=decay, scale=scale, is_training=training, updates_collections=None, scope=name)
 
-    ####################################################################################################################
-    ## ▣ dynamic_learning
-    ##  ⊙ epoch 가 클수록 아니면 early_stopping 이 시작되면 점차적으로 learning_rate의 값을 줄여 안정적인 훈련이 가능한 방법
-    ####################################################################################################################
-    def dynamic_learning(self,learning_rate,earlystop,epoch):
-        max_learning_rate = learning_rate
-        min_learing_rate = 0.001
-        learning_decay = 60 # 낮을수록 빨리 떨어진다.
-        if earlystop >= 1:
-            lr = min_learing_rate + (max_learning_rate - min_learing_rate) * np.exp(-epoch / learning_decay)
-        else:
-            lr = max_learning_rate
-        return round(lr,4)
+    
